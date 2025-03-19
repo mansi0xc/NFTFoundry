0ddb91c3f09ac0384ed12920cda115aee7376eb7
mansi0xc <mansi.tibrewal76@gmail.com>
2025-03-19 15:16:18 +0000

    v1

---
+++ test/BasicNFTTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+import {DeployBasicNFT} from "../script/DeployBasicNFT.sol";
+
+contract BasicNFTTest is Test {
+    DeployBasicNFT public deployer;
+    BasicNFT public basicNFT;
+    address public USER = makeAddr("user");
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNFT();
+        basicNFT = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNFT.name();
+        // assert(actualName == expectedName); gives error since == can only compare elementary data types, strings are complex in the sense that they are an array
+        assert(keccak256(abi.encodePacked(actualName)) == keccak256(abi.encodePacked(expectedName)));
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNFT.mintNFT(PUG);
+
+        assert(basicNFT.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(basicNFT.tokenURI(0))) == keccak256(abi.encodePacked(PUG)));
+    }
+}
